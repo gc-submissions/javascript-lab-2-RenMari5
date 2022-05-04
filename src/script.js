b5b293737b6c03817dc61dd1991ee7e621679d6b
Renee Paczkowski <rpaczkowski@gmail.com>
2022-05-03 23:01:06 -0400

    More work on fight function

--- src/script.js
+++ src/script.js
@@ -39,9 +39,10 @@ const fight = (player1, player2, player1Health, player2Health) => {
   while (true) {
     if (attacker === player1) {
       player2Health = attackPlayer(player2Health);
-      return logHealth(player2, player2Health);
+      console.log(logHealth(player2, player2Health));
     } else if (isDead(player2Health) === true) {
-      return logDeath(player1, player2);
+      console.log(logDeath(player1, player2));
     }
+    break;
   }
 };
